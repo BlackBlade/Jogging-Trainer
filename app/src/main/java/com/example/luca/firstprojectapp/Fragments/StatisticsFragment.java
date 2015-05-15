a86483a7710cb8b07f1e19acaf949aeb6c12871b
Luca Longobardi <luca.longobardi.93@gmail.com>
2015-05-15 19:02:51 +0200

    Modified and found a solution for graphs. Still missing the interface with the database. This feature will be added in the next update.

--- app/src/main/java/com/example/luca/firstprojectapp/Fragments/StatisticsFragment.java
+++ app/src/main/java/com/example/luca/firstprojectapp/Fragments/StatisticsFragment.java
@@ -81,46 +81,8 @@ public class StatisticsFragment extends Fragment implements DatabaseManager.IOnC
     }
 
     @Override
-    public void fillView(Cursor cur) {
+    public void fillView(Cursor cur, int position) {
         //TODO implement method to fill graph
     }
 
-    /**
-     * Inits the graph setting horizontal and vertical bounds
-     * In this case vertical bounds are setted to initial weight graph
-     * @param graph input graph to init
-     */
-    private void initGraph(GraphView graph){
-        if(graph != null){
-
-        //ora il grafo è preciso, basta scegliere dei valori equidistanti!
-            StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graph);
-            staticLabelsFormatter.setHorizontalLabels(new String[]{"", "Days"});
-            staticLabelsFormatter.setVerticalLabels(new String[]{"30", "60", "90", "120", "150"});
-            graph.getGridLabelRenderer().setNumHorizontalLabels(2);
-            graph.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
-
-            graph.getGridLabelRenderer().setGridStyle(GridLabelRenderer.GridStyle.HORIZONTAL);
-
-            graph.getViewport().setXAxisBoundsManual(true);
-            graph.getViewport().setMinX(1.0);
-            graph.getViewport().setMaxX(31.0);
-
-            graph.getViewport().setYAxisBoundsManual(true);
-            graph.getViewport().setMinY(30.0);
-            graph.getViewport().setMaxY(150.0);
-
-
-            //TODO add dynamic series
-            LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[]{
-                    new DataPoint(10.0,60.0),
-                    new DataPoint(16.0,75.0),
-                    new DataPoint(20.0,150.0)
-
-            });
-
-            graph.addSeries(series);
-
-        }
-    }
 }
