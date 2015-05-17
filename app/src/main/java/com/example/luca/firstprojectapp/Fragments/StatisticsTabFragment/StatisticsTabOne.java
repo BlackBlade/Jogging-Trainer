a81968342836c6a95cc383558df3ff04ef4f97bc
Mary <marycoopergd@gmail.com>
2015-05-17 17:42:30 +0200

    Modified layout colors, now for good. Added listener to the list view for swapping frahments.
    Conflicts:
    	.idea/workspace.xml
    	build/intermediates/model_data.bin

--- app/src/main/java/com/example/luca/firstprojectapp/Fragments/StatisticsTabFragment/StatisticsTabOne.java
+++ app/src/main/java/com/example/luca/firstprojectapp/Fragments/StatisticsTabFragment/StatisticsTabOne.java
@@ -2,10 +2,9 @@ package com.example.luca.firstprojectapp.Fragments.StatisticsTabFragment;
 
 import android.app.Activity;
 import android.database.Cursor;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
