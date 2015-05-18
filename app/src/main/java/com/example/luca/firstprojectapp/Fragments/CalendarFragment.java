ceb2df173bde413d03a897f74325deba60571261
BeepHawkins <matteo_orzes@live.it>
2015-05-18 20:55:51 +0200

    Merge branch 'master' of https://github.com/BlackBlade/Jogging-Trainer
    Conflicts:
    	app/src/main/java/com/example/luca/firstprojectapp/MainActivity.java
    	build/intermediates/model_data.bin

--- app/src/main/java/com/example/luca/firstprojectapp/Fragments/CalendarFragment.java
+++ app/src/main/java/com/example/luca/firstprojectapp/Fragments/CalendarFragment.java
@@ -1,7 +1,38 @@
 package com.example.luca.firstprojectapp.Fragments;
 
+import android.app.Fragment;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.luca.firstprojectapp.DatabaseManagement.DatabaseManager;
+import com.example.luca.firstprojectapp.Interfaces.IOnActivityCallback;
+import com.example.luca.firstprojectapp.R;
+// need to include library for datepicker
 /**
  * Created by MatteoOrzes on 18/05/2015.
  */
-public class CalendarFragment {
+public class CalendarFragment extends Fragment implements DatabaseManager.IOnCursorCallback/*,com.andexert.calendarlistview.library.DatePickerController*/{
+
+    private IOnActivityCallback listener;
+    //private CalendarListView calendarView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        container.removeAllViews();
+
+        final View view = inflater.inflate(R.layout.statistics_layout,container,false);
+
+        //calendarView = view.findViewById(R.id.calendar);
+
+        return view;
+
+    }
+    @Override
+    public void fillView(Cursor cur) {
+        //implement that later
+    }
 }
