b52215c5ec9ea5388b35cf4649fca78fe83b7222
BeepHawkins <matteo_orzes@live.it>
2015-05-18 21:22:36 +0200

    Merge branch 'master' of https://github.com/BlackBlade/Jogging-Trainer
    Conflicts:
    	.idea/gradle.xml
    	.idea/libraries/support_annotations_21_0_3.xml
    	.idea/libraries/support_v4_21_0_3.xml
    	.idea/misc.xml
    	build/intermediates/dex-cache/cache.xml
    	build/intermediates/model_data.bin

--- app/src/main/java/com/example/luca/firstprojectapp/Fragments/CalendarFragment.java
+++ app/src/main/java/com/example/luca/firstprojectapp/Fragments/CalendarFragment.java
@@ -7,17 +7,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.andexert.calendarlistview.library.DatePickerController;
 import com.example.luca.firstprojectapp.DatabaseManagement.DatabaseManager;
 import com.example.luca.firstprojectapp.Interfaces.IOnActivityCallback;
 import com.example.luca.firstprojectapp.R;
+import com.andexert.calendarlistview.library.DayPickerView;
+import com.andexert.calendarlistview.library.SimpleMonthAdapter;
+
+
 // need to include library for datepicker
 /**
  * Created by MatteoOrzes on 18/05/2015.
  */
-public class CalendarFragment extends Fragment implements DatabaseManager.IOnCursorCallback/*,com.andexert.calendarlistview.library.DatePickerController*/{
+public class CalendarFragment extends Fragment implements DatabaseManager.IOnCursorCallback, DatePickerController{
 
     private IOnActivityCallback listener;
-    //private CalendarListView calendarView;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -31,8 +36,28 @@ public class CalendarFragment extends Fragment implements DatabaseManager.IOnCur
         return view;
 
     }
-    @Override
+
     public void fillView(Cursor cur) {
         //implement that later
+    }
+
+    @Override
+    public void fillView(Cursor cur, int position) {
+
+    }
+
+    @Override
+    public int getMaxYear() {
+        return 0;
+    }
+
+    @Override
+    public void onDayOfMonthSelected(int i, int i2, int i3) {
+
+    }
+
+    @Override
+    public void onDateRangeSelected(SimpleMonthAdapter.SelectedDays<SimpleMonthAdapter.CalendarDay> calendarDaySelectedDays) {
+
     }
 }
