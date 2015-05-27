92a4d37294a8a8335722709ab767560b22961f07
BeepHawkins <matteo_orzes@live.it>
2015-05-27 18:28:32 +0200

    stupid error

--- app/src/main/java/com/example/luca/firstprojectapp/EditWeightnPlanActivity.java
+++ app/src/main/java/com/example/luca/firstprojectapp/EditWeightnPlanActivity.java
@@ -3,6 +3,7 @@ package com.example.luca.firstprojectapp;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
@@ -147,6 +148,11 @@ public class EditWeightnPlanActivity extends ActionBarActivity implements IOnAct
 
     @Override
     public FragmentManager getMySupportFragmentManager() {
+        return null;
+    }
+
+    @Override
+    public LocationManager getSystemService(){
         return null;
     }
 
