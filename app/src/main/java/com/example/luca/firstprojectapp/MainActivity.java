47cb26cdeade85c8c974eca6e1774a822aa6ee2d
BeepHawkins <matteo_orzes@live.it>
2015-06-19 18:19:14 +0200

    syncro push

--- app/src/main/java/com/example/luca/firstprojectapp/MainActivity.java
+++ app/src/main/java/com/example/luca/firstprojectapp/MainActivity.java
@@ -20,6 +20,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.luca.firstprojectapp.Adapters.NavItem;
 import com.example.luca.firstprojectapp.Adapters.SlideListAdapter;
